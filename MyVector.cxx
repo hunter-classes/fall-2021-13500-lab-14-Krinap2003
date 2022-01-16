52aba0ba9d93ab85bcd7b5a185de489a2ca01cac
Krina Patel <KRINA.PATEL96@myhunter.cuny.edu>
2022-01-16 18:13:06 -0500

    added comments

--- MyVector.cxx
+++ MyVector.cxx
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "MyVector.h"
 
+//Constructor method
 template<class T>
 MyVector<T>::MyVector()
 {
@@ -9,19 +10,21 @@ MyVector<T>::MyVector()
     arr = new T[startingCapacity];
 }
 
-
+//Returns the size of the vector
 template<class T>
 int MyVector<T>::size()
 {
     return s;
 }
 
+//Return the capacity
 template<class T>
 int MyVector<T>::capacity()
 {
     return startingCapacity;
 }
 
+//return true if the vector is empty and false otherwise
 template<class T>
 bool MyVector<T>::empty()
 {
@@ -37,6 +40,8 @@ bool MyVector<T>::empty()
     }
 }
 
+//make the last element in the vector the last element passed in as the 
+//parameter and increases the size of the vector
 template<class T>
 void MyVector<T>::push_back(T item)
 {
@@ -53,12 +58,14 @@ void MyVector<T>::push_back(T item)
     arr[s++] = item;
 }
 
+//return the element at index i in the vector 
 template<class T>
 T &MyVector<T>::operator[](int i)
 {
     return arr[i];
 }
 
+//delets the last element in the vector and reduces the size by 1
 template<class T>
 void MyVector<T>::pop_back()
 {
@@ -76,6 +83,7 @@ void MyVector<T>::pop_back()
 
 }
 
+//delets the element at index n in the vector and reduces the size by 1
 template<class T>
 void MyVector<T>::pop_back(int n)
 {
@@ -95,7 +103,7 @@ void MyVector<T>::pop_back(int n)
     arr = temp;
 }
 
-//description
+//delets all the elements in the vector
 template<class T>
 void MyVector<T>::clear()
 {
