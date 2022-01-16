6e3480ea8a85adab0c3b187d58cb8535d564e987
Krina Patel <KRINA.PATEL96@myhunter.cuny.edu>
2022-01-16 18:06:38 -0500

    added more tests

--- tests.cpp
+++ tests.cpp
@@ -24,3 +24,18 @@ TEST_CASE("t1")
     t1.clear();
     CHECK(t1.size() == 0);
 }
+
+TEST_CASE("t2")
+{
+    MyVector<int> t2;
+    CHECK(t2.empty() == true); 
+    t2.push_back(100);
+    t2.pop_back();
+    CHECK(t2.empty() == true); 
+    t2.push_back(1000000);
+    t2.push_back(25);
+    t2.push_back(6);
+    CHECK(t2.size() == 3);
+    t2.pop_back(1);
+    CHECK(t2[1]== 6);
+}
